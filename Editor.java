147b8acd6d59b8a37db674b7001653447f2c3be0
Roman <Nemchinovrp@yandex.ru>
2013-10-13 11:04:33 +0900

    Update Editor.java

--- Editor.java
+++ Editor.java
@@ -104,41 +104,6 @@ public class Editor extends JFrame {
 		color.setSelectedIndex(0);
 		fontListener();
 		
-			class MainItemAdapter implements ItemListener{
-
-				public void itemStateChanged(ItemEvent b) {
-					fontListener();
-					}
-			}
-		class MainActionListener implements ActionListener{
-			public void actionPerformed(ActionEvent a) {
-			String Label;
-			Label = a.getActionCommand();
-			if(Label.equals("Открыть"))
-				try {
-					fileLoad();
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			if(Label.equals("Сохранить"))
-				try {
-					fileRecord();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			if(Label.equals("Печать"))
-				filePrint();
-			if(Label.equals("Закрыть"))
-				System.exit(0);
-			if(Label.equals("Найти"))
-				stringSearch();
-			}
-			}
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		MainActionListener actionlistener = new MainActionListener();
@@ -281,53 +246,3 @@ public class Editor extends JFrame {
 	}
 	}
 
-	@SuppressWarnings("serial")
-	class PointDialog extends JDialog implements ActionListener{
-		private JTextField textfield;
-		private JButton ok, cut;
-		private String suchstring;
-		
-			PointDialog(JFrame f, String titel){
-				super(f, titel,true);
-				setResizable(false);
-				setLayout(new BorderLayout());
-				
-				JPanel panel1 = new JPanel();
-				JLabel label = new JLabel("Введите строку для поиска");
-				panel1.add(label);
-				textfield = new JTextField(40);
-				panel1.add(textfield);
-				add("Center", panel1);
-				JPanel panel2 = new JPanel();
-				ok = new JButton("ok");
-				cut = new JButton("Отмена");
-				panel2.add(ok);
-				panel2.add(cut);
-				add("South", panel2);
-				pack();
-				ok.addActionListener(this);
-				cut.addActionListener(this);
-				setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-			}
-
-			
-			public void actionPerformed(ActionEvent e) {
-				String Label;
-				Label = e.getActionCommand();
-				if(Label.equals("Отмена")){
-					suchstring =null;
-					setVisible(false);
-					return;
-			}
-			if(Label.equals("ok")){
-				suchstring =textfield.getText();
-				setVisible(false);
-				return;
-		}
-		
-		
-			}
-			public String getString(){
-				return suchstring;
-			}
-	}
